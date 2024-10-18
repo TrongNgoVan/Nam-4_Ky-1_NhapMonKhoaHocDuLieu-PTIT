4ee00432c2a8e4c3e0ec0f07c700fc367cab33d4
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2024-10-18 18:45:22 +0700

    trọng

--- BTL/Project_E-Commerce Growth Analysis(TikTokShop)/Chuanhoa/chuanhoa.py
+++ BTL/Project_E-Commerce Growth Analysis(TikTokShop)/Chuanhoa/chuanhoa.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-path = 'B:\Documents\DataScience Research(09 2024)\BTL_KHDL\Chuanhoa\PreprocessingData.csv'
+path = r'B:\Documents\Kỳ 1 - Năm 4\ChatRealtimeApp_JavaSocket_Ver1\udp_exam_scores.csv'
 
 data = pd.read_csv(path)
 
 df = pd.DataFrame(data)
 
-df.to_excel('B:\Documents\DataScience Research(09 2024)\BTL_KHDL\Chuanhoa\PreprocessingData.xlsx')
+df.to_excel(r'B:\Documents\Kỳ 1 - Năm 4\ChatRealtimeApp_JavaSocket_Ver1\udp_exam_scores.xlsx')
 print('Done')
